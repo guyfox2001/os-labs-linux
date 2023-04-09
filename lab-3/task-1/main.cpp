31986b1559a33750e2c97dc97362544654a70430
Boris <boris.stupin01@gmail.com>
2023-04-09 02:24:32 +0000

    upd

--- lab-3/task-1/main.cpp
+++ lab-3/task-1/main.cpp
@@ -7,16 +7,19 @@ using namespace std;
 int main(int argc, char** argv) {
     string file = argv[1];
     std::cout << file << std::endl;
-    ofstream out(file);
-    if(out.is_open()){
-        out << "Hello, ";
-        out.close();
-    }
+    ofstream out("");
+
     int child_pid = fork();
     if (child_pid == 0){
         out.open(file, ios_base::app);
         if(out.is_open()){
             out << "world;\n";
+            out.close();
+        }
+    }else{
+        out.open(file);
+        if(out.is_open()){
+            out << "Hello, ";
             out.close();
         }
     }
